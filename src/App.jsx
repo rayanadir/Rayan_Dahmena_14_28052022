b472792c8f3770ade5034d1ab7e6b3af9d18f8dc
rayanadir <rayan.dahmena@gmail.com>
2022-05-29 15:19:03 +0200

    correct errors

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import { Routes, Route } from 'react-router-dom';
 import CreateEmployee from './pages/CreateEmployee/CreateEmployee';
 import EmployeeList from './pages/EmployeeList/EmployeeList';
 
-const App = ()  => {
+const App = () => {
   return (
-    <body>
-      <Routes>
-        <Route path='/' element={<CreateEmployee />}/>
-        <Route path='/employee-list' element={<EmployeeList />}/>
-      </Routes>
-    </body>
+    <Routes>
+      <Route path='/' element={<CreateEmployee />} />
+      <Route path='/employee-list' element={<EmployeeList />} />
+    </Routes>
   );
 }
 
