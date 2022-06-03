0aba38957c1081cf6d41c6d90648a730f2cc4e7e
rayanadir <rayan.dahmena@gmail.com>
2022-06-03 22:57:28 +0200

    add columns const

---
+++ src/data/table_columns.js
@@ -0,0 +1,11 @@
+export const COLUMNS = [
+    { title: 'First Name', data: 'firstName', accessor: 'firstName' },
+    { title: 'Last Name', data: 'lastName', accessor: 'lastName' },
+    { title: 'Start Date', data: 'startDate', accessor: 'startDate' },
+    { title: 'Department', data: 'department', accessor: 'department' },
+    { title: 'Date of Birth', data: 'dateOfBirth', accessor: 'dateOfBirth' },
+    { title: 'Street', data: 'street', accessor: 'street' },
+    { title: 'City', data: 'city', accessor: 'city' },
+    { title: 'State', data: 'state', accessor: 'state' },
+    { title: 'Zip Code', data: 'zipCode', accessor: 'zipCode' },
+]
