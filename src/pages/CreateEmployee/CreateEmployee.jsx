b109957c5535c1972e11da80b90f2d19370df9dd
rayanadir <rayan.dahmena@gmail.com>
2022-06-05 19:18:16 +0200

    create new employee feature

--- src/pages/CreateEmployee/CreateEmployee.jsx
+++ src/pages/CreateEmployee/CreateEmployee.jsx
@@ -3,14 +3,38 @@ import { Link } from 'react-router-dom'
 import Select from '../../components/select/Select';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { useSelector } from "react-redux"
 
 const CreateEmployee = () => {
     document.title = "HRnet"
+    
     const saveEmployee = () => {
-        console.log("save employee");
+        const employees = JSON.parse(localStorage.getItem('employees')) || [];
+        const employee={
+            firstName,
+            lastName,
+            birthdate,
+            startdate,
+            street,
+            city,
+            state,
+            zipCode,
+            department
+        }
+        employees.push(employee);
+        localStorage.setItem('employees', JSON.stringify(employees));
     }
-    const [birthdate, onChangeBirthdate] = useState(undefined);
-    const [startdate, onChangeStartdate] = useState(undefined);
+
+    const [firstName, setFirstName]= useState("");
+    const [lastName, setLastName]= useState("");
+    const [birthdate, onChangeBirthdate] = useState(null);
+    const [startdate, onChangeStartdate] = useState(null);
+    const [street, setStreet] = useState("");
+    const [city, setCity]= useState("");
+    const state = useSelector((state) => state.state.state);
+    const [zipCode, setZipCode] = useState(null);
+    const department = useSelector((state) => state.department.department)
+
     return (
         <React.Fragment>
             <header className="title">
@@ -20,18 +44,18 @@ const CreateEmployee = () => {
                 <section className="container">
                     <Link to='/employee-list'>View Current Employees</Link>
                     <h2>Create Employee</h2>
-                    <form action="#" id="create-employee">
+                    <form action='#' id="create-employee" onSubmit={()=> {saveEmployee()}}>
                         <label htmlFor="first-name">First Name</label>
-                        <input type="text" id="first-name" required />
+                        <input type="text" id="first-name" required onChange={(e) => {setFirstName(e.target.value)}}/>
 
                         <label htmlFor="last-name">Last Name</label>
-                        <input type="text" id="last-name" required />
+                        <input type="text" id="last-name" required onChange={(e) => {setLastName(e.target.value)}}/>
 
                         <label htmlFor="date-of-birth">Date of Birth</label>
                         <DatePicker
                             value={birthdate}
                             selected={birthdate}
-                            onChange={(date) => onChangeBirthdate(date)}
+                            onChange={(date) => {onChangeBirthdate(date)}}
                             className='date-input'
                             id='date-of-birth'
                             required
@@ -60,27 +84,22 @@ const CreateEmployee = () => {
                             <legend>Address</legend>
 
                             <label htmlFor="street">Street</label>
-                            <input id="street" type="text" required />
+                            <input id="street" type="text" required onChange={(e) => {setStreet(e.target.value)}}/>
 
                             <label htmlFor="city">City</label>
-                            <input id="city" type="text" required />
+                            <input id="city" type="text" required onChange={(e) => {setCity(e.target.value)}}/>
 
                             <label htmlFor="state">State</label>
-                            <Select />
+                            <Select name="state"/>
 
                             <label htmlFor="zip-code">Zip Code</label>
-                            <input id="zip-code" type="number" required />
+                            <input id="zip-code" type="number" required onChange={(e) => {setZipCode(e.target.value)}}/>
                         </fieldset>
 
                         <label htmlFor="department">Department</label>
-                        <select name="department" id="department" required>
-                            <option>Sales</option>
-                            <option>Marketing</option>
-                            <option>Engineering</option>
-                            <option>Human Resources</option>
-                            <option>Legal</option>
-                        </select>
-                        <button onClick={saveEmployee} type='submit' className='submit'>Save</button>
+                        <Select name="department" />
+                        
+                        <button type='submit' className='submit'>Save</button>
                     </form>
                     <div id="confirmation" className="modal">Employee Created!</div>
                 </section>
