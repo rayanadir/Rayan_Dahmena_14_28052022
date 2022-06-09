05a23f0045c053392c534f5d76a79b3bd2c74edb
rayanadir <rayan.dahmena@gmail.com>
2022-06-09 19:39:31 +0200

    change page style

--- src/pages/CreateEmployee/CreateEmployee.jsx
+++ src/pages/CreateEmployee/CreateEmployee.jsx
@@ -1,107 +1,150 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import Select from '../../components/select/Select';
+//import Select from '../../components/select/Select';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
+import { states } from '../../data/states';
+import { departments } from '../../data/departments';
+import Select from 'react-select';
+import { stateSelected } from '../../slices/statesSlice';
+import { departmentSelected } from '../../slices/departmentSlice';
 
 const CreateEmployee = () => {
     document.title = "HRnet"
-    
-    const saveEmployee = () => {
-        const employees = JSON.parse(localStorage.getItem('employees')) || [];
-        const employee={
-            firstName,
-            lastName,
-            birthdate,
-            startdate,
-            street,
-            city,
-            state,
-            zipCode,
-            department
+
+    const dispatch = useDispatch()
+
+    const saveEmployee = (e) => {
+        e.preventDefault();
+        if (isStateSelected === false) {
+            document.getElementById("state-required").style.display = "block"
         }
-        employees.push(employee);
-        localStorage.setItem('employees', JSON.stringify(employees));
+        if (isDepartmentSelected === false) {
+            document.getElementById("department-required").style.display = "block"
+        }
+        if (isStateSelected && isDepartmentSelected) {
+            const employees = JSON.parse(localStorage.getItem('employees')) || [];
+            const employee = {
+                firstName,
+                lastName,
+                birthdate,
+                startdate,
+                street,
+                city,
+                state,
+                zipCode,
+                department
+            }
+            employees.push(employee);
+            localStorage.setItem('employees', JSON.stringify(employees));
+            console.log(employees);
+        }
     }
 
-    const [firstName, setFirstName]= useState("");
-    const [lastName, setLastName]= useState("");
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
     const [birthdate, onChangeBirthdate] = useState(null);
     const [startdate, onChangeStartdate] = useState(null);
     const [street, setStreet] = useState("");
-    const [city, setCity]= useState("");
-    const state = useSelector((state) => state.state.state);
+    const [city, setCity] = useState("");
+    const isStateSelected = useSelector(state => state.state.selected)
+    const [state, setState] = useState(null)
     const [zipCode, setZipCode] = useState(null);
-    const department = useSelector((state) => state.department.department)
+    const isDepartmentSelected = useSelector((state) => state.department.selected)
+    const [department, setDepartment] = useState(null)
+    useSelector((state) => {
+        if (state.state.selected === true) {
+            document.getElementById("state-required").style.display = "none"
+        }
+        if (state.department.selected === true) {
+            document.getElementById("department-required").style.display = "none"
+        }
+    });
+
+    const STATES = [...new Set(states.map((state) => { return { value: state.abbreviation, label: state.name } }))];
+    const DEPARTMENTS = [...new Set(departments.map((department) => { return { value: department.name, label: department.name } }))]
+
 
     return (
         <React.Fragment>
             <header className="title">
                 <h1>HRnet</h1>
+                <Link className='link' to='/employee-list'>View Current Employees</Link>
             </header>
             <main>
                 <section className="container">
-                    <Link to='/employee-list'>View Current Employees</Link>
                     <h2>Create Employee</h2>
-                    <form action='#' id="create-employee" onSubmit={()=> {saveEmployee()}}>
-                        <label htmlFor="first-name">First Name</label>
-                        <input type="text" id="first-name" required onChange={(e) => {setFirstName(e.target.value)}}/>
+                    <form action='#' id="create-employee" onSubmit={(e) => { saveEmployee(e) }}>
 
-                        <label htmlFor="last-name">Last Name</label>
-                        <input type="text" id="last-name" required onChange={(e) => {setLastName(e.target.value)}}/>
+                        <div className='input-address'>
+                            <div className='text-date-input'>
+                                <label htmlFor="first-name">First Name</label>
+                                <input type="text" id="first-name" required onChange={(e) => { setFirstName(e.target.value) }} />
 
-                        <label htmlFor="date-of-birth">Date of Birth</label>
-                        <DatePicker
-                            value={birthdate}
-                            selected={birthdate}
-                            onChange={(date) => {onChangeBirthdate(date)}}
-                            className='date-input'
-                            id='date-of-birth'
-                            required
-                            showYearDropdown
-                            dateFormat="dd/MM/yyyy"
-                            scrollableYearDropdown
-                            maxDate={new Date()}
-                            yearDropdownItemNumber={new Date().getFullYear()-1900}
-                        />
+                                <label htmlFor="last-name">Last Name</label>
+                                <input type="text" id="last-name" required onChange={(e) => { setLastName(e.target.value) }} />
 
-                        <label htmlFor="start-date">Start Date</label>
-                        <DatePicker
-                            value={startdate}
-                            selected={startdate}
-                            onChange={(date) => onChangeStartdate(date)}
-                            className='date-input'
-                            id='start-date'
-                            required
-                            showYearDropdown
-                            dateFormat="dd/MM/yyyy"
-                            yearDropdownItemNumber={30}
-                            scrollableYearDropdown
-                        />
+                                <label htmlFor="date-of-birth">Date of Birth</label>
+                                <DatePicker
+                                    value={birthdate}
+                                    selected={birthdate}
+                                    onChange={(date) => { onChangeBirthdate(date) }}
+                                    className='date-input'
+                                    id='date-of-birth'
+                                    required
+                                    showYearDropdown
+                                    dateFormat="dd/MM/yyyy"
+                                    scrollableYearDropdown
+                                    maxDate={new Date()}
+                                    yearDropdownItemNumber={new Date().getFullYear() - 1900}
+                                />
 
-                        <fieldset className="address">
-                            <legend>Address</legend>
+                                <label htmlFor="start-date">Start Date</label>
+                                <DatePicker
+                                    value={startdate}
+                                    selected={startdate}
+                                    onChange={(date) => onChangeStartdate(date)}
+                                    className='date-input'
+                                    id='start-date'
+                                    required
+                                    showYearDropdown
+                                    dateFormat="dd/MM/yyyy"
+                                    yearDropdownItemNumber={30}
+                                    scrollableYearDropdown
+                                />
 
-                            <label htmlFor="street">Street</label>
-                            <input id="street" type="text" required onChange={(e) => {setStreet(e.target.value)}}/>
 
-                            <label htmlFor="city">City</label>
-                            <input id="city" type="text" required onChange={(e) => {setCity(e.target.value)}}/>
+                                <label htmlFor="department">Department</label>
+                                {/*<Select name="department" id="department" required={true} options={DEPARTMENTS} placeholder="Select a department" />*/}
+                                <Select onChange={(e) => { setDepartment(e.label); dispatch(departmentSelected()) }} options={DEPARTMENTS} placeholder="Select a department" className='select' isMulti={false} />
+                                <p className='select-required' id='department-required'>Please select a department</p>
 
-                            <label htmlFor="state">State</label>
-                            <Select name="state"/>
+                            </div>
 
-                            <label htmlFor="zip-code">Zip Code</label>
-                            <input id="zip-code" type="number" required onChange={(e) => {setZipCode(e.target.value)}}/>
-                        </fieldset>
+                            <fieldset className="address">
+                                <legend>Address</legend>
 
-                        <label htmlFor="department">Department</label>
-                        <Select name="department" />
-                        
-                        <button type='submit' className='submit'>Save</button>
+                                <label htmlFor="street">Street</label>
+                                <input id="street" type="text" required onChange={(e) => { setStreet(e.target.value) }} />
+
+                                <label htmlFor="city">City</label>
+                                <input id="city" type="text" required onChange={(e) => { setCity(e.target.value) }} />
+
+                                <label htmlFor="state">State</label>
+                                {/*<Select id="state" name="state" options={STATES} required={true} placeholder="Select a state" />*/}
+                                <Select onChange={(e) => { setState(e.label); dispatch(stateSelected()) }} options={STATES} placeholder="Select a state" className='select' isMulti={false} />
+                                <p className='select-required' id='state-required'>Please select a state</p>
+
+                                <label htmlFor="zip-code">Zip Code</label>
+                                <input id="zip-code" type="number" required onChange={(e) => { setZipCode(e.target.value) }} />
+                            </fieldset>
+                        </div>
+                        <div className='department_submit'>
+                            <button type='submit' className='submit'>Save</button>
+                        </div>
                     </form>
-                    <div id="confirmation" className="modal">Employee Created!</div>
+                    {/*<div id="confirmation" className="modal">Employee Created!</div>*/}
                 </section>
             </main>
         </React.Fragment>
