85ecd1fbe1badbfadea9052812077169a45963ac
rayanadir <rayan.dahmena@gmail.com>
2022-06-10 22:59:09 +0200

    update table columns

--- src/data/table_columns.js
+++ src/data/table_columns.js
@@ -1,11 +1,58 @@
 export const COLUMNS = [
-    { title: 'First Name', data: 'firstName', accessor: 'firstName' },
-    { title: 'Last Name', data: 'lastName', accessor: 'lastName' },
-    { title: 'Start Date', data: 'startDate', accessor: 'startDate' },
-    { title: 'Department', data: 'department', accessor: 'department' },
-    { title: 'Date of Birth', data: 'dateOfBirth', accessor: 'dateOfBirth' },
-    { title: 'Street', data: 'street', accessor: 'street' },
-    { title: 'City', data: 'city', accessor: 'city' },
-    { title: 'State', data: 'state', accessor: 'state' },
-    { title: 'Zip Code', data: 'zipCode', accessor: 'zipCode' },
-]
+    {
+      field: 'firstName',
+      headerName: 'First name',
+      width: 120,
+      editable: false,
+    },
+    {
+      field: 'lastName',
+      headerName: 'Last name',
+      width: 120,
+      editable: false,
+    },
+    {
+      field: 'startDate',
+      headerName: 'Start Date',
+      type: 'date',
+      width: 120,
+      editable: false,
+    },
+    {
+      field: 'department',
+      headerName: 'Department',
+      width: 120,
+      editable: false,
+    },
+    {
+      field: 'dateOfBirth',
+      headerName: 'Date of Birth',
+      type: 'date',
+      width: 120,
+      editable: false,
+    },
+    {
+      field: 'street',
+      headerName: 'Street',
+      width: 120,
+      editable: false,
+    },
+    {
+      field: 'city',
+      headerName: 'City',
+      width: 120,
+      editable: false,
+    },
+    {
+      field: 'state',
+      headerName: 'State',
+      width: 120,
+      editable: false,
+    },
+    {
+      field: 'zipCode',
+      headerName: 'Zip Code',
+      width: 120,
+      editable: false,
+    },
+  ];
