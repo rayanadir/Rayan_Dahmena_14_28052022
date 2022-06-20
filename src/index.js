c33df55fb99ec4fbcaf2bfd629c7b28feac164e3
rayanadir <88830520+rayanadir@users.noreply.github.com>
2022-06-20 18:55:09 +0200

    change browserrouter into hashrouter

--- src/index.js
+++ src/index.js
@@ -3,20 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
-import { store } from './app/store'; 
+import { store } from './app/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
     <React.Fragment>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.Fragment>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </React.Fragment>
   </Provider>
-  
+
 );
 
 // If you want to start measuring performance in your app, pass a function
