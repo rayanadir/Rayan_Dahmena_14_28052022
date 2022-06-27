f9c9c78cd54d7becca87c581c00ad311dc484171
rayanadir <88830520+rayanadir@users.noreply.github.com>
2022-06-27 11:48:53 +0200

    update store

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,13 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { selectStateReducer } from "../slices/statesSlice";
 import { selectDepartmentReducer } from "../slices/departmentSlice";
+import { employeesStateReducer } from "../slices/employeesSlice";
 
 export const store = configureStore({
     reducer:{
         state: selectStateReducer,
-        department : selectDepartmentReducer
+        department : selectDepartmentReducer,
+        employees: employeesStateReducer,
     },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({serializableCheck: false})
 })
