e4958d418ec9b28bf51ff15fea24b5ae90d51230
rayanadir <rayan.dahmena@gmail.com>
2022-09-24 15:50:02 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-<<<<<<< HEAD
 import { HashRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
 import { store } from './app/store';
@@ -11,16 +10,10 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist';
 
 let persistor = persistStore(store)
-=======
-import { BrowserRouter, HashRouter } from "react-router-dom";
-import { Provider } from 'react-redux';
-import { store } from './app/store';
->>>>>>> f9c9c78cd54d7becca87c581c00ad311dc484171
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-<<<<<<< HEAD
     <PersistGate loading={null} persistor={persistor}>
       <React.Fragment>
         <HashRouter>
@@ -28,13 +21,6 @@ root.render(
         </HashRouter>
       </React.Fragment>
     </PersistGate>
-=======
-    <React.Fragment>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </React.Fragment>
->>>>>>> f9c9c78cd54d7becca87c581c00ad311dc484171
   </Provider>
 
 );
--- src/pages/CreateEmployee/CreateEmployee.jsx
+++ src/pages/CreateEmployee/CreateEmployee.jsx
@@ -1,14 +1,11 @@
-<<<<<<< HEAD
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import { useSelector, useDispatch } from "react-redux"
+import { useDispatch } from "react-redux"
 import { states } from '../../data/states';
 import { departments } from '../../data/departments';
 import Select from 'react-select';
-import { stateSelected, resetSelectedState } from '../../slices/statesSlice';
-import { departmentSelected, resetSelectDepartment } from '../../slices/departmentSlice';
 import { addEmployee } from '../../slices/employeesSlice'
 
 import {useModal, Modal} from "react-modal-library-rayan-dahmena"
@@ -32,10 +29,10 @@ const CreateEmployee = () => {
     const [startdate, onChangeStartdate] = useState(undefined);
     const [street, setStreet] = useState("");
     const [city, setCity] = useState("");
-    const [isStateSelected, setIsStateSelected] = useState(false) //useSelector(state => state.state.selected)
+    const [isStateSelected, setIsStateSelected] = useState(false)
     const [state, setState] = useState(undefined)
     const [zipCode, setZipCode] = useState(undefined);
-    const [isDepartmentSelected, setIsDepartmentSelected] = useState(false) //useSelector((state) => state.department.selected)
+    const [isDepartmentSelected, setIsDepartmentSelected] = useState(false) 
     const [department, setDepartment] = useState(undefined)
 
     const [deptErr, setDeptErr] = useState("");
@@ -44,11 +41,9 @@ const CreateEmployee = () => {
     const saveEmployee = (e) => {
         e.preventDefault();
         if (isStateSelected === false) {
-            //document.getElementById("state-required").style.display = "block"
             setStateErr("err")
         }
         if (isDepartmentSelected === false) {
-            //document.getElementById("department-required").style.display = "block"
             setDeptErr("err")
         }
         console.log(isStateSelected + ' ' + isDepartmentSelected);
@@ -163,161 +158,4 @@ const CreateEmployee = () => {
     )
 }
 
-=======
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import { useSelector, useDispatch } from "react-redux"
-import { states } from '../../data/states';
-import { departments } from '../../data/departments';
-import Select from 'react-select';
-import { stateSelected } from '../../slices/statesSlice';
-import { departmentSelected } from '../../slices/departmentSlice';
-import { addEmployee } from '../../slices/employeesSlice'
-
-import {useModal, Modal} from "react-modal-library-rayan-dahmena"
-import ElementModal from '../../components/ElementModal/ElementModal';
-
-/**
- * returns the CreateEmployee page
- * @returns {JSX}
- */
-const CreateEmployee = () => {
-    document.title = "HRnet"
-
-    const dispatch = useDispatch()
-    const { isShowing, toggle } = useModal()
-
-    const saveEmployee = (e) => {
-        e.preventDefault();
-        if (isStateSelected === false) {
-            document.getElementById("state-required").style.display = "block"
-        }
-        if (isDepartmentSelected === false) {
-            document.getElementById("department-required").style.display = "block"
-        }
-        if (isStateSelected && isDepartmentSelected) {
-            const employee = {
-                firstName,
-                lastName,
-                birthdate,
-                startdate,
-                street,
-                city,
-                state,
-                zipCode,
-                department
-            }
-            dispatch(addEmployee(employee))
-            toggle();
-        }
-    }
-    
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [birthdate, onChangeBirthdate] = useState(null);
-    const [startdate, onChangeStartdate] = useState(null);
-    const [street, setStreet] = useState("");
-    const [city, setCity] = useState("");
-    const isStateSelected = useSelector(state => state.state.selected)
-    const [state, setState] = useState(null)
-    const [zipCode, setZipCode] = useState(null);
-    const isDepartmentSelected = useSelector((state) => state.department.selected)
-    const [department, setDepartment] = useState(null)
-    useSelector((state) => {
-        if (state.state.selected === true) {
-            document.getElementById("state-required").style.display = "none"
-        }
-        if (state.department.selected === true) {
-            document.getElementById("department-required").style.display = "none"
-        }
-    });
-
-    const STATES = [...new Set(states.map((state) => { return { value: state.abbreviation, label: state.name } }))];
-    const DEPARTMENTS = [...new Set(departments.map((department) => { return { value: department.name, label: department.name } }))]
-
-    return (
-        <React.Fragment>
-            <header className="title">
-                <h1>HRnet</h1>
-                <Link className='link' to='/employee-list'>View Current Employees</Link>
-            </header>
-            <main>
-                <section className="container">
-                    <h2>Create Employee</h2>
-                    <form action='#' id="create-employee" onSubmit={(e) => { saveEmployee(e) }}>
-
-                        <div className='input-address'>
-                            <div className='text-date-input'>
-                                <label htmlFor="first-name">First Name</label>
-                                <input type="text" id="first-name" required onChange={(e) => { setFirstName(e.target.value) }} />
-
-                                <label htmlFor="last-name">Last Name</label>
-                                <input type="text" id="last-name" required onChange={(e) => { setLastName(e.target.value) }} />
-
-                                <label htmlFor="date-of-birth">Date of Birth</label>
-                                <DatePicker
-                                    value={birthdate}
-                                    selected={birthdate}
-                                    onChange={(date) => { onChangeBirthdate(date) }}
-                                    className='date-input'
-                                    id='date-of-birth'
-                                    required
-                                    showYearDropdown
-                                    dateFormat="dd/MM/yyyy"
-                                    scrollableYearDropdown
-                                    maxDate={new Date()}
-                                    yearDropdownItemNumber={new Date().getFullYear() - 1900}
-                                />
-
-                                <label htmlFor="start-date">Start Date</label>
-                                <DatePicker
-                                    value={startdate}
-                                    selected={startdate}
-                                    onChange={(date) => onChangeStartdate(date)}
-                                    className='date-input'
-                                    id='start-date'
-                                    required
-                                    showYearDropdown
-                                    dateFormat="dd/MM/yyyy"
-                                    yearDropdownItemNumber={30}
-                                    scrollableYearDropdown
-                                />
-
-                                <label htmlFor="department">Department</label>
-                                <Select onChange={(e) => { setDepartment(e.label); dispatch(departmentSelected()) }} options={DEPARTMENTS} placeholder="Select a department" className='select' isMulti={false} />
-                                <p className='select-required' id='department-required'>Please select a department</p>
-
-                            </div>
-
-                            <fieldset className="address">
-                                <legend>Address</legend>
-
-                                <label htmlFor="street">Street</label>
-                                <input id="street" type="text" required onChange={(e) => { setStreet(e.target.value) }} />
-
-                                <label htmlFor="city">City</label>
-                                <input id="city" type="text" required onChange={(e) => { setCity(e.target.value) }} />
-
-                                <label htmlFor="state">State</label>
-                                <Select onChange={(e) => { setState(e.label); dispatch(stateSelected()) }} options={STATES} placeholder="Select a state" className='select' isMulti={false} />
-                                <p className='select-required' id='state-required'>Please select a state</p>
-
-                                <label htmlFor="zip-code">Zip Code</label>
-                                <input id="zip-code" type="number" required onChange={(e) => { setZipCode(e.target.value) }} />
-                            </fieldset>
-                        </div>
-                        <div className='department_submit'>
-                            <button onClick={(e)=> {saveEmployee(e)}} type="submit" className='submit'>Save</button>
-                        </div>
-                    </form>
-                    <Modal element={<ElementModal />} isShowing={isShowing} toggle={toggle} />
-                </section>
-            </main>
-        </React.Fragment>
-    )
-}
-
->>>>>>> f9c9c78cd54d7becca87c581c00ad311dc484171
 export default CreateEmployee
